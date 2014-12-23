ca1197108476d718d701fdfb718ddb8d45f5b05e
Chen, Chia-Ho <Chia-Ho.Chen@nfl.com>
2014-12-23 14:39:53 -0800

    test

--- app/src/main/java/com/example/chia_hochen/ribbit/RibbitApplication.java
+++ app/src/main/java/com/example/chia_hochen/ribbit/RibbitApplication.java
@@ -2,17 +2,20 @@ package com.example.chia_hochen.ribbit;
 
 import android.app.Application;
 
+import com.parse.Parse;
+import com.parse.ParseObject;
+
 /**
  * Created by chia-ho.chen on 12/22/14.
  */
 public class RibbitApplication extends Application {
 
+    @Override
     public void onCreate() {
         super.onCreate();
 
         Parse.enableLocalDatastore(this);
 
         Parse.initialize(this, "v8nRtGAEodwS4YBsrFvkAw3oYBC1kWCzEc0LT9VY", "DYDqOruxAwd94GPlYsNRJZxfUewAIu41TIVSDe3s");
-
     }
 }
